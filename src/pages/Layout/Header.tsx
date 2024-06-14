08d9901f0de9835ef2ad8084065a3436f10fb6b8
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-14 16:44:18 -0400

    basic Layout done

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -1,3 +1,13 @@
+import Container from '../../components/Container'
+
 export default function Header() {
-	return <header>Header</header>
+	return (
+		<header className='py-3 border shadow-md'>
+			<Container>
+				<div className='flex justify-between items-center'>
+					<h1 className='text-2xl font-semibold'>Cuenta Pan</h1>
+				</div>
+			</Container>
+		</header>
+	)
 }
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,15 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
-import MainContainer from './MainContainer'
 
 export default function Layout() {
 	return (
 		<>
 			<Header />
-			<MainContainer>
+			<main>
 				<Outlet />
-			</MainContainer>
+			</main>
 			<Footer />
 		</>
 	)
