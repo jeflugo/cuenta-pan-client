5a707cb8164822376d961c4c725f21d36f3ed4e6
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-14 16:42:50 -0400

    recipe contants added

---
+++ src/constants/index.ts
@@ -0,0 +1,67 @@
+const SALTY_REC = {
+	flour: {
+		unit: 'g',
+		amount: 30000,
+	},
+	water: {
+		unit: 'g',
+		amount: 14000,
+	},
+	sugar: {
+		unit: 'g',
+		amount: 1500,
+	},
+	salt: {
+		unit: 'g',
+		amount: 560,
+	},
+	butter: {
+		unit: 'g',
+		amount: 300,
+	},
+	vanilla: {
+		unit: 'tapa',
+		amount: 3,
+	},
+	butterEssence: {
+		unit: 'tapa',
+		amount: 3,
+	},
+	margarineEssence: {
+		unit: 'tapa',
+		amount: 3,
+	},
+}
+
+const SWEET_REC = {
+	flour: {
+		unit: 'g',
+		amount: 30000,
+	},
+	water: {
+		unit: 'g',
+		amount: 12000,
+	},
+	sugar: {
+		unit: 'g',
+		amount: 6800,
+	},
+	salt: {
+		unit: 'g',
+		amount: 200,
+	},
+	butter: {
+		unit: 'g',
+		amount: 300,
+	},
+	vanillaEssence: {
+		unit: 'tapa',
+		amount: 4,
+	},
+	pineappleEssence: {
+		unit: 'tapa',
+		amount: 4,
+	},
+}
+
+export { SALTY_REC, SWEET_REC }
