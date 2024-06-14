3a0d15d8dfd563ea70f15feadb61ee91216437fd
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-14 16:43:39 -0400

    MainContainer removed

--- src/pages/Layout/MainContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type TProps = {
-	children: React.ReactElement | null
-}
-
-export default function MainContainer({ children }: TProps) {
-	return <main>{children}</main>
-}
