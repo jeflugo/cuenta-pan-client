4e50e8ca59bf02ee75e45e7d565c3718d8935d28
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-15 20:36:06 -0400

    Toaster added

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
+import { Toaster } from 'react-hot-toast'
 
 export default function Layout() {
 	return (
 		<>
+			<Toaster />
 			<Header />
 			<main>
 				<Outlet />
