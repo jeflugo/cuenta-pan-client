cf62a1c25e86c2c3116ddfe072c07f626502a64b
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-17 22:45:36 -0400

    Bread list now has and modal and the prep is correctly displayed

--- src/pages/Home/BreadList.tsx
+++ src/pages/Home/BreadList.tsx
@@ -2,12 +2,15 @@ import { Button } from '@material-tailwind/react'
 import { useStateContext } from '../../context/state-context'
 import { StateContextType, TBread, TPrep } from '../../lib/types'
 import BreadField from './BreadField'
+import AddBread from './AddBread'
+import { useState } from 'react'
 
 type BreadListProps = {
 	tag: string
 }
 
 export default function BreadList({ tag }: BreadListProps) {
+	const [addOpen, setAddOpen] = useState(false)
 	const {
 		saltyBreads,
 		setSaltyBreads,
@@ -35,12 +38,14 @@ export default function BreadList({ tag }: BreadListProps) {
 		name = 'Salado'
 	}
 
+	const toggleAdd = () => setAddOpen(!addOpen)
+
 	return (
 		<div className='mb-6'>
 			<h2 className='text-xl font-medium mb-1'>Pan {name}</h2>
 
 			{breads && (
-				<div className='mb-4'>
+				<div className='mb-4 gap-2 flex flex-col'>
 					{breads.map(bread => {
 						return (
 							<BreadField
@@ -57,7 +62,7 @@ export default function BreadList({ tag }: BreadListProps) {
 			<div
 				className={`w-full flex ${breads ? 'justify-end' : 'justify-center'} mb-1`}
 			>
-				<Button size='sm' variant='outlined'>
+				<Button size='sm' variant='outlined' onClick={toggleAdd}>
 					Añadir nuevo pan
 				</Button>
 			</div>
@@ -68,49 +73,59 @@ export default function BreadList({ tag }: BreadListProps) {
 						calculateMass(tag)
 					}}
 				>
-					Calcular Masa
+					Calcular Preparacion
 				</Button>
 			)}
+			{addOpen && <AddBread toggleAdd={toggleAdd} tag={tag} />}
 			{prep && (
-				<div>
-					<div>
-						Masa total: {prep.mass.amount} {prep.mass.unit}
-					</div>
-					<div>
-						Harina: {prep.flour.amount} {prep.flour.unit}
-					</div>
-					<div>
-						Agua: {prep.water.amount} {prep.water.unit}
-					</div>
-					<div>
-						Azucar: {prep.sugar.amount} {prep.sugar.unit}
-					</div>
-					<div>
-						Sal: {prep.salt.amount} {prep.salt.unit}
-					</div>
-					<div>
-						Manteca: {prep.butter.amount} {prep.butter.unit}
-					</div>
-					<div>
-						Vainilla: {prep.vanilla.amount} {prep.vanilla.unit}
-					</div>
-					{tag === 'sweet' ? (
-						<div>
-							E. piña: {prep.pineappleEssence!.amount}{' '}
-							{prep.pineappleEssence!.unit}
-						</div>
-					) : (
-						<>
-							<div>
-								E. mantecado: {prep.butterEssence!.amount}{' '}
-								{prep.butterEssence!.unit}
-							</div>
-							<div>
-								E. mantequilla: {prep.margarineEssence!.amount}{' '}
-								{prep.margarineEssence!.unit}
-							</div>
-						</>
-					)}
+				<div className='mt-4'>
+					<h3 className='text-lg font-bold underline'>Preparacion</h3>
+					<ul className='list-disc ml-5'>
+						<li>
+							Masa total: {prep.mass.amount} {prep.mass.unit}
+						</li>
+						<li>
+							Harina: {prep.flour.amount} {prep.flour.unit}
+						</li>
+						<li>
+							Agua: {prep.water.amount} {prep.water.unit}
+						</li>
+						<li>
+							Azucar: {prep.sugar.amount} {prep.sugar.unit}
+						</li>
+						<li>
+							Sal: {prep.salt.amount} {prep.salt.unit}
+						</li>
+						<li>
+							Manteca: {prep.butter.amount} {prep.butter.unit}
+						</li>
+						<li>
+							Vainilla: {prep.vanilla.amount} {prep.vanilla.unit}
+						</li>
+						{tag === 'sweet' ? (
+							<>
+								<li>
+									E. piña: {prep.pineappleEssence!.amount}{' '}
+									{prep.pineappleEssence!.unit}
+								</li>
+								<li>Canela: a su criterio</li>
+								<li>Anis chiquito: a su criterio</li>
+								<li>Colorante: a su criterio</li>
+							</>
+						) : (
+							<>
+								<li>
+									E. mantecado: {prep.butterEssence!.amount}{' '}
+									{prep.butterEssence!.unit}
+								</li>
+								<li>
+									E. mantequilla: {prep.margarineEssence!.amount}{' '}
+									{prep.margarineEssence!.unit}
+								</li>
+							</>
+						)}
+						<li>Levadura: a su criterio</li>
+					</ul>
 				</div>
 			)}
 		</div>
