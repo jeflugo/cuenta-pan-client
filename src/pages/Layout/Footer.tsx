461a6876d63613b31d118eb0c7acacb6e7fd8545
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-17 22:43:48 -0400

    footer done

--- src/pages/Layout/Footer.tsx
+++ src/pages/Layout/Footer.tsx
@@ -1,3 +1,15 @@
+import Container from '../../components/Container'
+
 export default function Footer() {
-	return <footer>Footer</footer>
+	return (
+		<footer className='mt-auto border border-black '>
+			<Container>
+				<div className='py-3'>
+					<h4 className='text-center'>
+						Creado por <span className='font-bold'>Jeferson</span>
+					</h4>
+				</div>
+			</Container>
+		</footer>
+	)
 }
