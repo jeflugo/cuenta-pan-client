518bcce86f82c371e0c649bf2399fff48d911cdc
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-24 21:20:40 -0400

    dnd funtionality added with @dnd-kit

--- src/pages/Home/BreadList.tsx
+++ src/pages/Home/BreadList.tsx
@@ -1,6 +1,20 @@
 import React, { Suspense, lazy } from 'react'
 import { TBread } from '../../lib/types'
 import Loading from '../../components/Loading'
+import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core'
+import {
+	arrayMove,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	useSortable,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import {
+	KeyboardSensor,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from '@dnd-kit/core'
 const Bread = lazy(() => import('./Bread'))
 
 type BreadListProps = {
@@ -9,23 +23,82 @@ type BreadListProps = {
 	LSBreads: string
 }
 
+function SortableItem({
+	bread,
+	breads,
+	setBreads,
+	LSBreads,
+}: {
+	bread: TBread
+	breads: TBread[]
+	setBreads: React.Dispatch<React.SetStateAction<TBread[] | null>>
+	LSBreads: string
+}) {
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable({ id: bread.id })
+
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	}
+
+	return (
+		<div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+			<Suspense fallback={<Loading paddingY='5' />}>
+				<Bread
+					bread={bread}
+					breads={breads}
+					setBreads={setBreads}
+					LSBreads={LSBreads}
+				/>
+			</Suspense>
+		</div>
+	)
+}
+
 export default function BreadList({
 	breads,
 	setBreads,
 	LSBreads,
 }: BreadListProps) {
+	const sensors = useSensors(
+		useSensor(PointerSensor),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		})
+	)
+
+	const handleDragEnd = (event: DragEndEvent) => {
+		const { active, over } = event
+		if (active.id !== over?.id) {
+			setBreads(items => {
+				if (!items) return null
+				const oldIndex = items.findIndex(item => item.id === active.id)
+				const newIndex = items.findIndex(item => item.id === over?.id)
+				return arrayMove(items, oldIndex, newIndex)
+			})
+		}
+	}
+
 	return (
-		<div className='mb-4 gap-2 flex flex-col'>
-			{breads.map(bread => (
-				<Suspense key={bread.id} fallback={<Loading paddingY='5' />}>
-					<Bread
-						bread={bread}
-						breads={breads}
-						setBreads={setBreads}
-						LSBreads={LSBreads}
-					/>
-				</Suspense>
-			))}
-		</div>
+		<DndContext
+			sensors={sensors}
+			collisionDetection={closestCenter}
+			onDragEnd={handleDragEnd}
+		>
+			<SortableContext items={breads.map(bread => bread.id)}>
+				<div className='mb-4 gap-2 flex flex-col'>
+					{breads.map(bread => (
+						<SortableItem
+							key={bread.id}
+							bread={bread}
+							breads={breads}
+							setBreads={setBreads}
+							LSBreads={LSBreads}
+						/>
+					))}
+				</div>
+			</SortableContext>
+		</DndContext>
 	)
 }
