c7e0e81afd637d4c766fbf34e8f126575310f143
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-02 21:29:22 -0400

    styles changed

--- src/pages/Home/DeleteBread.tsx
+++ src/pages/Home/DeleteBread.tsx
@@ -14,12 +14,17 @@ export default function DeleteBread({
 	return (
 		<div className='fixed z-10 top-0 left-0 right-0 bottom-0 bg-black/50 flex justify-center items-center'>
 			<div className='py-4 px-6 bg-white rounded'>
-				<h3 className='text-lg mb-4'>Eliminar "{name}" de la lista?</h3>
+				<h3 className='text-lg mb-2'>Eliminar "{name}" de la lista?</h3>
 				<div className='flex gap-2 justify-end'>
 					<Button size='sm' onClick={toggleDelete}>
 						Cancelar
 					</Button>
-					<Button size='sm' color='red' onClick={deleteBread}>
+					<Button
+						size='sm'
+						color='red'
+						variant='outlined'
+						onClick={deleteBread}
+					>
 						Eliminar
 					</Button>
 				</div>
